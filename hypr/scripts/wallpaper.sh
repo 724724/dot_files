44c839960d966c3568d19d75415b295bbbf6e6fc
724724 <slee2452@wisc.edu>
2025-12-29 14:31:31 -0600

    feat: hyprpaper 삭제 및 swww로 변경

---
+++ hypr/scripts/wallpaper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 데몬이 이미 실행 중인지 확인하고 없으면 실행
+if ! pgrep -x "swww-daemon" > /dev/null; then
+    swww-daemon &
+    sleep 0.5 
+fi
+
+# 배경화면 설정 (기존 contain 옵션 -> --resize fit)
+# 특정 모니터만 지정하고 싶다면 --outputs "eDP-1" 등을 추가하면 됩니다.
+swww img /home/sejunlee/junk/wallpaper/wallpaper.png --resize fit
